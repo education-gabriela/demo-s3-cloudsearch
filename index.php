439ec8a1d0039877aed809ea80db3899496b35e1
gabidavila <gabidavila@gmail.com>
2015-02-19 11:32:55 +0000

    fixed config path

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'bootstrap.php';
 
 use Gabidavila\S3cloudsearch\Models\Twitter;
 
-$twitter = new Twitter($twitter_config);
+$twitter = new Twitter($twitter);
 
 //$twitter->get('/statuses/user_timeline', '?screen_name=gabidavila&count=2');
 $twitter->add('gabidavila');
